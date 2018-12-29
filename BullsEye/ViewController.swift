0a3d573b2038a61b0bc88aed4d1384789d4a7ae7
Craig Booker <craig@arcadiasoftworks.com>
2018-12-29 14:55:48 -0600

    Added algorithm for calculating the difference between target and slider value.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-        var currentValue: Int = 0
-        var targetValue: Int = 0
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var roundValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     @IBOutlet weak var infoButton: UIButton!
     @IBOutlet weak var startOverButton: UIButton!
@@ -18,9 +22,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreNumericValue: UILabel!
     @IBOutlet weak var roundNumberValue: UILabel!
     @IBOutlet weak var roundTextLabel: UILabel!
-    @IBOutlet weak var randomTargetValue: UILabel!
+
     @IBOutlet weak var minSliderValue: UILabel!
-    @IBOutlet weak var slider: UISlider!
+  
 
     
     @IBOutlet weak var maxSliderValue: UILabel!
@@ -28,24 +32,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
-        targetValue = Int.random(in: 1...100)
-        randomTargetValue.text = String(targetValue)
+        startNewRound()
+        
         
     }
     @IBAction func showAlert(){
-        let message = "The value of the slider is now: \(currentValue)" + " \n The target value is now: \(targetValue)"
+        
+        var difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        
+        let message = "The value of the slider is now: \(currentValue)" +
+        " \n The target value is: \(targetValue)" +
+        " \n The difference is: \(difference)"
+        
         let alert = UIAlertController(title: "Hellow, World!", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
-        
+        updateLabels()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
